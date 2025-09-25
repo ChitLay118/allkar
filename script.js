767cdcb492fcc4e8e58eeb8ccaa87f1fc5a13b54
ChitLay118 <c388925@gmail.com>
2025-09-25 16:47:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const sites = [
   { name: "SarBalGyi", url: "https://sarbalgyi.net", category: "ချောင်းရိုက်", desc: "မြန်မာ", domain: "sarbalgyi.net" },
   { name: "မြန်မာစာတန်းထိုးဖူးကား1", url: "https://darkplustv.com/category/uncategorized/", category: "mmsub", desc: "Japan", domain: "darkplustv.com" },
   { name: "မြန်မာစာတန်းထိုးဖူးကား2", url: "https://xxxmmsub.com/", category: "mmsub", desc: "Japan", domain: "xxxmmsub.com" },
-  { name: "PRED(mmsub)", url: "https://javmyanmar.com/pred-722/", category: "mmsub", desc: "Japan", domain: "javmyanmar.com/pred-722/" }
+  { name: "PRED(mmsub)", url: "https://javmyanmar.com/pred-722/", category: "mmsub", desc: "Japan", domain: "javmyanmar.com/pred-722/" },
+  { name: "MMSUB", url: "https://mmsub.org/", category: "mmsub", desc: "all", domain: "mmsub.org" }
 ];
 
 // grid element
@@ -77,6 +78,7 @@ render(sites);
    - sites array ကို ဖိုင်ရှင်းပေါ်က JSON ဖိုင်တစ်ခုနဲ့ ထည့်ချင်တယ်ဆိုရင် fetch() သုံးပြီး ပြောင်းလဲနိုင်သည်။
    - Icon မပြနိုင်ပါက default placeholder ထည့်လိုပါက makeCard() ထဲမှာ စစ်ပြီး image.onerror သတ်မှတ်နိုင်သည်။
 */
+
 
 
 
